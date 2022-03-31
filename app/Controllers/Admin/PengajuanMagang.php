67808fb02fcc0a7048a9de5d86b53528ad680a56
afiffarishudaifi <afiffaris5@gmail.com>
2022-03-31 14:21:33 +0700

    update

--- app/Controllers/Admin/PengajuanMagang.php
+++ app/Controllers/Admin/PengajuanMagang.php
@@ -47,6 +47,30 @@ class PengajuanMagang extends BaseController
         $id = $this->request->getPost('id_pengajuan');
         $id_siswa = $this->request->getPost('id_siswa');
 
+        if($this->request->getPost('status_pengajuan') == 'Ditolak') {
+            $email = \Config\Services::email();
+            $email->setFrom('bakesbangpoldagrikabmadiun@gmail.com','Badan Kesatuan Bangsa dan Politik Dalam Negeri Kabupaten Madiun');
+            $email->setTo('ditamega0411@gmail.com');
+
+            // $email->attach(base_url('docs/img/img_logo/1643622777_07e1231c2b3237328d8d.gif'));
+
+            $email->setSubject('Pemberitahuan Pengajuan Magang');
+            $email->setMessage('Dengan ini kami melakukan Penolakan atas pengajuan magang pada kantor Badan Kesatuan Bangsa dan Politik Dalam Negeri Kabupaten Madiun pada tanggal 12-12-2020.');
+
+            $email->send();
+        } else {
+            $email = \Config\Services::email();
+            $email->setFrom('bakesbangpoldagrikabmadiun@gmail.com','Badan Kesatuan Bangsa dan Politik Dalam Negeri Kabupaten Madiun');
+            $email->setTo('ditamega0411@gmail.com');
+
+            $email->attach(base_url('docs/img/img_logo/1643622777_07e1231c2b3237328d8d.gif'));
+
+            $email->setSubject('Pemberitahuan Pengajuan Magang');
+            $email->setMessage('Dengan ini kami melakukan konfirmasi atas pengajuan magang pada kantor Badan Kesatuan Bangsa dan Politik Dalam Negeri Kabupaten Madiun pada tanggal 12-12-2020. Bersama ini kami kirimkan surat penerimaan pengajuan magang oleh saudara Dita Mega Saputri dari Politeknik Negeri Madiun. Untuk mengkonfirmasi pihak Badan Kesatuan Bangsa dan Politik Dalam Negeri, saudara diharapakan datang ke kantor Badan Kesatuan Bangsa dan Politik Dalam Negeri Kabupaten Madiun untuk membahas rencana magang paling lambat pada tanggal 12-02-2022.');
+
+            $email->send();
+        }
+
         $data = array(
             'id_admin'          => $session->get('id_login'),
             'status_pengajuan'      => $this->request->getVar('status_pengajuan'),
